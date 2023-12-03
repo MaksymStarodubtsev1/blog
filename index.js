4e29c1f8a87bf2305c7055b517031713a065b962
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2023-12-03 14:33:01 +0200

    server: added jwt token generation

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
 app.post('/auth/login', (req, res) => {
+
+    const token = jwt.sign({
+        email: req.body.email,
+        name: req.body.name,
+    }, 'super_secret')
+
     res.json({
         success: true,
+        token,
     })
 })
 
