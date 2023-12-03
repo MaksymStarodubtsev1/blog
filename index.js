84cf9cff7a1aeabfb36ecbd9245c0cf4b051da99
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2023-12-03 13:56:59 +0200

    server: added listener on root route

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express()
 
-app.listen('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello world')
+})
+
+
+app.listen(3333, (error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('Server Ok');
+    }
 })
