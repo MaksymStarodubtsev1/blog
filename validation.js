7ba17bcd1a2825e0596a79d9e66e4bf988f9a50a
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2024-01-01 18:38:52 +0200

    server: added post validation

--- validation.js
+++ validation.js
@@ -5,4 +5,11 @@ export const authValidator = [
     body('password').isLength({min: 5}),
     body('fullName').isLength({min: 3}),
     body('avatarUrl').optional().isURL(),
-]
+];
+
+export const postValidation = [
+    body('title', 'Please enter post title').isLength({min: 3}).isString(),
+    body('text', 'Please enter text for post').isLength({min: 3}).isString(),
+    body('tags', 'Enter correct format (should be array)').optional().isString(),
+    body('imageUrl', 'Provide correct url').optional().isURL(),
+];
