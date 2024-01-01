dfcd09a705e79b43880abbdb643abfcbcc02d514
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2024-01-01 18:21:41 +0200

    server: moved controllers to separate folder

---
+++ controllers/UserControllers.js
@@ -0,0 +1,91 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator"
+import UserScheme from "../modules/User.js"
+
+export const login = async (req, res) => {
+    try {
+        // Gettin user if exist
+        const user = await UserScheme.findOne({ email: req.body.email })
+
+        // Comparing user request with data in DB
+        if(!user) {
+            res.status(404).json('wrong info, try again');
+        }
+        const isPasswordValid = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if(!isPasswordValid) {
+            return res.status(401).json('wrong parrword or login');
+        }
+    
+        // Logination Success
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SALT);
+        
+        // Return user info with token
+        res.json({
+            ...user._doc,
+            token: token,
+        });
+
+    } catch (error) {
+        res.status(505).json('wrong auth info')
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+
+        // Getting user by id
+        const user = await UserScheme.findById(req.id)
+
+        if(!user) {
+            res.status(403).json('Anuthorized')
+        }
+    
+        const { passwordHash, ...userData } = user._doc
+
+        // Reterning user info
+        res.json({
+            ...userData,
+        });
+    } catch (error) {
+        res.status(403).json(error)
+    }
+}
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req)
+
+        // Checking request info on Errors
+        if(!errors.isEmpty()) {
+            res.status(400).json(errors.array())
+        }
+    
+        // Genereting password hash
+        const password = req.body.password;
+        const salt = bcrypt.genSaltSync(10)
+        const passwordHash = await bcrypt.hash(password, salt)
+    
+        // Creating User Scheme
+        const document = new UserScheme({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            passwordHash,
+            avatarUrl: req.body.avatarUrl || ''
+        })
+    
+        const user = await document.save()
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SALT);
+    
+        // Registration Success
+        res.json({
+            ...user._doc,
+            token: token,
+        });
+
+    } catch(error) {
+        res.status(500).json('Error appear, try again with other names')
+    }
+}
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import authMiddlevare from "./utils/authMiddlevare.js";
 import * as dotenv from "dotenv";
 import { authValidator } from './validation/auth.js'
 
-import { validationResult } from "express-validator"
-import UserScheme from "./modules/User.js"
+import { register,getMe, login } from "./controllers/UserControllers.js";
 
 dotenv.config()
 
@@ -26,92 +23,11 @@ app.get('/', (req, res) => {
 })
 
 // Checking if user includes in request by authMiddlevare
-app.get('/auth/me', authMiddlevare, async (req, res) => {
-    try {
+app.get('/auth/me', authMiddlevare, getMe)
 
-        // Getting user by id
-        const user = await UserScheme.findById(req.id)
+app.post('/auth/login', login)
 
-        if(!user) {
-            res.status(403).json('Anuthorized')
-        }
-    
-        const { passwordHash, ...userData } = user._doc
-
-        // Reterning user info
-        res.json({
-            ...userData,
-        });
-    } catch (error) {
-        res.status(403).json(error)
-    }
-})
-
-app.post('/auth/login', async (req, res) => {
-    try {
-        // Gettin user if exist
-        const user = await UserScheme.findOne({ email: req.body.email })
-
-        // Comparing user request with data in DB
-        if(!user) {
-            res.status(404).json('wrong info, try again');
-        }
-        const isPasswordValid = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-        if(!isPasswordValid) {
-            return res.status(401).json('wrong parrword or login');
-        }
-    
-        // Logination Success
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SALT);
-        
-        // Return user info with token
-        res.json({
-            ...user._doc,
-            token: token,
-        });
-
-    } catch (error) {
-        res.status(505).json('wrong auth info')
-    }
-})
-
-app.post('/auth/register', authValidator, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-
-        // Checking request info on Errors
-        if(!errors.isEmpty()) {
-            res.status(400).json(errors.array())
-        }
-    
-        // Genereting password hash
-        const password = req.body.password;
-        const salt = bcrypt.genSaltSync(10)
-        const passwordHash = await bcrypt.hash(password, salt)
-    
-        // Creating User Scheme
-        const document = new UserScheme({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            passwordHash,
-            avatarUrl: req.body.avatarUrl || ''
-        })
-    
-        const user = await document.save()
-
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SALT);
-    
-        // Registration Success
-        res.json({
-            ...user._doc,
-            token: token,
-        });
-
-    } catch(error) {
-        res.status(500).json('Error appear, try again with other names')
-    }
-})
+app.post('/auth/register', register)
 
 
 app.listen(3333, (error) => {
