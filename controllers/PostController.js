8e9feee7f41e7bd3766e81c09bc4624cfd59c6fd
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2024-01-03 22:39:16 +0200

    server: added Post controller for specific post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,12 +4,35 @@ import { validationResult } from "express-validator"
 
 export const getAll = async (req, res) => {
     try {
-        const doc = await PostModel.find().populate('user')
+        const doc = await PostModel.find().populate('user').exec()
 
         res.json(doc)
 
     } catch(error) {
         res.status(500).json('Error appear, try again with other info')
+    }
+}
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const doc = await PostModel.findByIdAndUpdate(
+            {
+                _id: postId,
+            },
+            {
+                $inc: {viewCount: 1},
+            },
+            {
+                returnDocument: 'after',
+            }
+        ).exec()
+        
+        res.json(doc)
+
+    } catch(error) {
+        res.status(404).json('Error appear, no info found')
     }
 }
 
